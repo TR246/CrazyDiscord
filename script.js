ebbcadc9860839ff7a8301c77ca23830b394bc06
TR246 <m.ikezawa.12@gmail.com>
2020-09-29 21:51:52 +0900

    コピー機能実装

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ window.addEventListener("DOMContentLoaded", () => {
                     const { value } = e.target;
                     if (value == lastInput) {
                         lastLength = value.length;
-                    } else if (Math.random() < 0.8) {
+                    } else if (Math.random() < 0.6) {
                         result += value.slice(lastLength);
                     }
                     lastInput = value;
@@ -39,6 +39,7 @@ window.addEventListener("DOMContentLoaded", () => {
                 };
                 const confirm = () => {
                     box_result.innerText = result;
+                    buttonCopy.disabled = false;
                     textarea.removeEventListener("keypress", confirmByEnter);
                     buttonConfirm.removeEventListener("click", confirm);
                 };
@@ -53,7 +54,25 @@ window.addEventListener("DOMContentLoaded", () => {
         removeCurrentListener();
         textarea.value = "";
         box_result.innerText = "入力してください";
+        buttonCopy.disabled = true;
         reset();
     });
+
+    buttonCopy.addEventListener("click", () => {
+        /*const tmp = document.createElement("textarea");
+        tmp.value = box_result.innerText;
+        tmp.select();
+        document.execCommand("copy");*/
+        document.addEventListener(
+            "copy",
+            (e) => {
+                e.preventDefault();
+                e.clipboardData.setData("text/plain", box_result.innerText);
+            },
+            { once: true }
+        );
+        document.execCommand("copy");
+    });
+
     reset();
 });
